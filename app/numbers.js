ea9fa3433db68cfa1c4b40055780fb8098e33460
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-08-13 13:57:17 -0600

    finished numbers

--- app/numbers.js
+++ app/numbers.js
@@ -1,22 +1,23 @@
 // Write a function that uses the addition operator to add two numbers together
 function add(x, y) {
-
+  return x + y;
 }
 
 // Write a function that returns the Cubed value of x
 function cubed(x) {
-
+  return Math.pow(x, 3);
 }
 
 // Write a function that will divide two numbers, and will format the returned value
 // as a whole number. (No Decimals)
 function divideWholey(x, y) {
-
+  return Math.floor(x / y);
 }
 
 // Write a function that takes in a number and determines if it is even. 
 // It should return a boolean value - true if it is even, false if it is odd.
 function isEven(num) {
+  return (num % 2 == 0 ? true : false);
 }
 
 
@@ -24,4 +25,7 @@ function isEven(num) {
 // Make it inclusive so that the starting and ending numbers are part 
 // of the range.HINT ** Utilize Javascript's Math Class (To the Google!)
 function numberGenerator(num1, num2) {
+  let max = Math.max(num1, num2);
+  let min = Math.min(num1, num2);
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
